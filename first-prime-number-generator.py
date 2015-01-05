df66031eea987dbc98757b341e4ae2a4b4aa660d
Joey Brown <joey.brown@lindseysoftware.com>
2015-01-05 16:49:32 -0600

    make generator

--- first-prime-number-generator.py
+++ first-prime-number-generator.py
@@ -1,2 +1,17 @@
 # Return First Prime Number
-2
+class first_prime_number:
+    def __init__(self):
+        self.n = 2
+
+    def __iter__(self):
+        return self
+
+    # Python 3 compatibility
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        return self.n
+
+generator = first_prime_number()
+print(generator.next())
